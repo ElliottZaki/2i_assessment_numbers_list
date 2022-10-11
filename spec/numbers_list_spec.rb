92c52749eedc69453da7699b357f2c96c3de0223
Elliott Zaki <elliottzaki@Elliotts-MacBook-Air.local>
2022-10-11 18:34:45 +0100

    Fifith passing test. The sort_in_descending_order method sorts an array and arranges it into a descending format. This test is for a ten element array.

--- spec/numbers_list_spec.rb
+++ spec/numbers_list_spec.rb
@@ -16,8 +16,12 @@ describe Numbers_List do
     end
 
     describe '#sort_in_descending_order' do
-        it 'should sort an array in a descending order' do
+        it 'should sort an array of two elements in a descending order' do
             expect(subject.sort_in_descending_order([1, 2])).to eq [2, 1]
+        end
+
+        it 'should sort an array of ten elements in a descending order' do
+            expect(subject.sort_in_descending_order([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to eq [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
         end
     end
 end
