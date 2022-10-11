89d2dbdf18b12a32d1519bfb5cd75b0010616621
Elliott Zaki <elliottzaki@Elliotts-MacBook-Air.local>
2022-10-11 18:21:57 +0100

    Second passing test. The organise array method returns an array of two elements when passed an array of two elements.

--- spec/numbers_list_spec.rb
+++ spec/numbers_list_spec.rb
@@ -2,8 +2,12 @@ require './lib/numbers_list'
 
 describe Numbers_List do
     describe '#organise_array' do
-        it 'should  return an array of 1 element' do
+        it 'should  return an array of one element when passed an array of one element' do
             expect(subject.organise_array([1])).to eq [1]
+        end
+
+        it 'should  return an array of two elements when passed an array of two elements' do
+            expect(subject.organise_array([1, 2])).to eq [1, 2]
         end
     end
 end
