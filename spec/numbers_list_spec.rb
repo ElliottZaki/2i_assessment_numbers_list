836f57991540b30df966bd4c07a09ca5a313dbbd
Elliott Zaki <elliottzaki@Elliotts-MacBook-Air.local>
2022-10-11 18:23:24 +0100

    Third passing test. Organise array method return an array of ten elements when passed an array of ten elements.

--- spec/numbers_list_spec.rb
+++ spec/numbers_list_spec.rb
@@ -6,8 +6,12 @@ describe Numbers_List do
             expect(subject.organise_array([1])).to eq [1]
         end
 
-        it 'should  return an array of two elements when passed an array of two elements' do
+        it 'should return an array of two elements when passed an array of two elements' do
             expect(subject.organise_array([1, 2])).to eq [1, 2]
+        end
+
+        it 'should return an array of ten elements when passed an array of ten elements' do
+            expect(subject.organise_array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to eq [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         end
     end
 end
